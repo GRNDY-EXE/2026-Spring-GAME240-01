ad951f5be8c1e6d3a7936ca506ac79ee81c438b4
GRNDY-EXE <andwang@chapman.edu>
2026-04-02 23:32:40 -0700

    march 12 assignment

---
+++ homework/march_9/submission/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] numbers = new int[] { 3, 5, 6, 4, 8, 5, 10, 9, 7, 2, 3 };
+int i;
+i = 1;
+int largest;
+largest = numbers[0];
+
+
+while (i < numbers.Length)
+{
+    if (numbers[i] > largest)
+    {
+        largest = numbers[i];
+        i = i + 1;
+    } else if (numbers[i] < largest)
+    {
+        i = i + 1;
+    }
+}
+
+Console.WriteLine("The largest number is " + largest);
